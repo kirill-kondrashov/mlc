e5d294dc30e0771e5e40c161cde4c906d95fad6b
Kirill Kondrashov <kondrashovks1989@gmail.com>
2025-12-24 14:33:00 +0100

    Fix `norm_growth`

--- Mlc/Quadratic/Complex/Escape.lean
+++ Mlc/Quadratic/Complex/Escape.lean
@@ -23,11 +23,14 @@ lemma norm_sq_sub_norm_c_ge (c z : ℂ) : ‖z^2‖ - ‖c‖ ≥ ‖z‖^2 - (R
   simp only [norm_sq]
   linarith [R_ge_one_plus_c c]
 
-lemma norm_growth (c z : ℂ) (h : ‖z‖ > R c) : ‖fc c z‖ ≥ ‖z‖^2 - (R c - 1) := by
-  rw [fc, ← norm_sq]
-  have : ‖z^2 + c‖ ≥ ‖z^2‖ - ‖c‖ := by sorry
-  rw [norm_sq] at this
-  sorry
+lemma norm_growth (c z : ℂ) : ‖fc c z‖ ≥ ‖z‖^2 - (R c - 1) := by
+  rw [fc]
+  have h_tri : ‖z^2 + c‖ ≥ ‖z^2‖ - ‖c‖ := by
+    have := norm_add_le (z^2 + c) (-c)
+    simp only [add_neg_cancel_right, norm_neg] at this
+    linarith
+  rw [norm_sq] at h_tri
+  linarith [R_ge_one_plus_c c]
 
 /-- Escape lemma: if the orbit of z ever leaves the disk of radius R(c), then it
 escapes to infinity. -/
