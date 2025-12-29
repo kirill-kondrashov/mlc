895bd8b0a592fa710047870f9125f30c6cd83025
Kirill Kondrashov <kondrashovks1989@gmail.com>
2025-12-30 00:18:56 +0100

    Cleanup and verify (#8)

--- check_axioms.lean
+++ check_axioms.lean
@@ -1,2 +1,13 @@
 import Mlc.MainConjecture
-#print axioms MLC.MLC_Conjecture
+import Lean
+
+open Lean Meta Elab Command
+
+run_cmd liftTermElabM do
+  let name := ``MLC.MLC_Conjecture
+  let axioms ← Lean.collectAxioms name
+  if axioms.contains ``sorryAx then
+    throwError "❌ The proof of '{name}' relies on 'sorry'!"
+  else
+    logInfo s!"✅ The proof of '{name}' is free of 'sorry'."
+    logInfo s!"All axioms used: {axioms.toList}"
