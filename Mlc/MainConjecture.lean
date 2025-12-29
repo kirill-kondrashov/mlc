a639aae4187182b66cc54d147720912553fe5dc2
Kirill Kondrashov <kondrashovks1989@gmail.com>
2025-12-29 21:52:19 +0100

    ensure_no_sorry MLC.MLC_Conjecture

--- Mlc/MainConjecture.lean
+++ Mlc/MainConjecture.lean
@@ -130,4 +130,4 @@ ensure_no_sorry MLC.dichotomy
 ensure_no_sorry MLC.parameter_shrink
 
 -- Verify that the main conjecture does not depend on sorry
--- ensure_no_sorry MLC.MLC_Conjecture
+ensure_no_sorry MLC.MLC_Conjecture
